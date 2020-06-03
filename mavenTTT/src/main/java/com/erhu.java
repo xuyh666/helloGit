c557d5e588082a2e39f0980368e8ade2e3d5851c
xuyh666 <1029855397@qq.com>
2020-06-03 22:24:08 +0800

    更新2

--- mavenTTT/src/main/java/com/erhu.java
+++ mavenTTT/src/main/java/com/erhu.java
@@ -3,5 +3,7 @@ package com;
 public class erhu {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
